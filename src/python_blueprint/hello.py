10e17e681447271052f336814d4b367642f1dadd
Salvatore D'Angelo <sasadangelo@gmail.com>
2026-05-06 15:42:11 +0200

    Move project to Ruff and use Pytest for unit tests

--- src/python_blueprint/hello.py
+++ src/python_blueprint/hello.py
@@ -4,4 +4,4 @@ def say_hello(name: str) -> str:
 
 
 if __name__ == "__main__":
-    print(say_hello("World"))
+    print(say_hello(name="World"))
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,14 +1,7 @@
-import unittest
-
 from python_blueprint.hello import say_hello
 
 
-class TestHello(unittest.TestCase):
-    def test_say_hello(self):
-        self.assertEqual(say_hello("World"), "Hello, World!")
-        self.assertEqual(say_hello("Alice"), "Hello, Alice!")
-        self.assertNotEqual(say_hello("Bob"), "Hello, Alice!")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_say_hello() -> None:
+    assert say_hello(name="World") == "Hello, World!"
+    assert say_hello(name="Alice") == "Hello, Alice!"
+    assert say_hello(name="Bob") != "Hello, Alice!"
